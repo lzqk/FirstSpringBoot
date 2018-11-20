a6a45f01dc90a6b2df4380c75e7a921428f6429a
lzqk <18317893780@163.com>
2018-11-20 14:08:21 +0800

    tset 一下加个换行

--- demo1/src/main/java/com/example/demo/DemoApplication.java
+++ demo1/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
     /**
      * 
      * @param args
+     *
      */
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
