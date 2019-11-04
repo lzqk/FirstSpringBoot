00ead9d669b5c5aeb191a2d1aff9f0470ebf4b6c
lzqk <18317893780@163.com>
2019-11-04 16:08:12 +0800

    添加新项目

---
+++ elastic/src/test/java/com/lzqk/elastic/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzqk.elastic.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
