e08a23ab3870f851734f065ae08f9a9f56c1f50e
lzqk <18317893780@163.com>
2020-06-28 17:03:30 +0800

    Revert "commit 1"
    This reverts commit c233bf6d

--- demo1/src/main/java/com/example/demo/DemoApplication.java
+++ demo1/src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,9 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
-@EnableAsync
-//入口类
 public class DemoApplication {
     /**
      * spring boot 入口类
