188f533152b0f26bc56144da8b273d5b6044ee1e
lzqk <18317893780@163.com>
2020-06-28 17:04:11 +0800

    Revert "commit 2"
    This reverts commit 21dcdbeb

--- demo1/src/main/java/com/example/demo/DemoApplication.java
+++ demo1/src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,11 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
+@EnableAsync
 public class DemoApplication {
     /**
      * spring boot 入口类
